9df09d515ada07aadb6dfb2a78f6a4de248ea565
DESKTOP-BVHDEK4\Davor <pernjek.davor12@gmail.com>
2023-01-21 16:09:15 +0100

    add more tests and group by description

--- script.test.js
+++ script.test.js
@@ -2,12 +2,23 @@ const googleSearch = require("./script");
 
 dbMock = ["dog.com", "disney.com", "cheese.com", "dogcutepics.com"];
 
-it("is a silly test", () => {
-  expect("hello").toBe("hello");
-  googleSearch("testtest", dbMock);
-});
+describe("googleSearch", () => {
+  it("is a silly test", () => {
+    expect("hello").toBe("hello");
+    googleSearch("testtest", dbMock);
+  });
 
-it("is searching google", () => {
-  expect(googleSearch("testtest", dbMock)).toEqual([]);
-  expect(googleSearch("dog.com", dbMock)).toEqual(["dog.com"]);
+  it("is searching google", () => {
+    expect(googleSearch("testtest", dbMock)).toEqual([]);
+    expect(googleSearch("dog.com", dbMock)).toEqual(["dog.com"]);
+  });
+
+  it("work with undefined and null input", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it("doesn't return more than 3 matches", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(3);
+  });
 });
