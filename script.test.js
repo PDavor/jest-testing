31829e1bc151dd5f0d8311c67f3d20329d857926
DESKTOP-BVHDEK4\Davor <pernjek.davor12@gmail.com>
2023-01-21 16:05:05 +0100

    add simple test

--- script.test.js
+++ script.test.js
@@ -1,6 +1,13 @@
 const googleSearch = require("./script");
 
 dbMock = ["dog.com", "disney.com", "cheese.com", "dogcutepics.com"];
-it("this is a test", () => {
+
+it("is a silly test", () => {
+  expect("hello").toBe("hello");
   googleSearch("testtest", dbMock);
+});
+
+it("is searching google", () => {
+  expect(googleSearch("testtest", dbMock)).toEqual([]);
+  expect(googleSearch("dog.com", dbMock)).toEqual(["dog.com"]);
 });
