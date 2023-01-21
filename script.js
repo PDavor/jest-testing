d5bba055a795e5d235657dc026922aa1e66154f9
DESKTOP-BVHDEK4\Davor <pernjek.davor12@gmail.com>
2023-01-21 16:04:55 +0100

    read from db paramtere

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const googleDatabase = [
 ];
 
 const googleSearch = (searchInput, db) => {
-  const matches = googleDatabase.filter((website) => {
+  const matches = db.filter((website) => {
     return website.includes(searchInput);
   });
   return matches.length > 3 ? matches.slice(0, 3) : matches;
